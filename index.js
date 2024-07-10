e48e49b826ec1f157f6917e501cc51e59af24d1a
Rahul kumar <rahulsingh96287@gmail.com>
2024-07-10 22:14:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+const app= express()
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+mongoose.connect('mongodb://localhost:27017/MoneyList')
+const db = mongoose.connection
+db.on('error', ()=> console.log("Error in connecting to the Database"))
+db.once('open', () => console.log("Connected to Database"))
+
+app.post("/add", (req,res) =>{
+    const category_select = req.body.category_select
+    const amount_input= req.body.amount_input
+    const info = req.body.info
+    const date_input = req.body.date_input
+
+    const data={
+        "Category": category_select,
+        "Amount" : amount_input,
+        "Info": info,
+        "Date": date_input
+    }
+    db.collection('users').insertOne(data, (err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully")
+
+    })
+})
+app.get("/",(req,res) =>{
+    res.set({
+        "Allow-access-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+}).listen(3000)
+
+console.log("Listening on port 3000")
